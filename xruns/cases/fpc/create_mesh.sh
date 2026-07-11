50694ebf976d0c3a2fb5682d827d5863788702d6
biswajit <biswajitkhara@gmail.com>
2026-07-11 18:58:38 -0500

    some minor changes

--- xruns/cases/fpc/create_mesh.sh
+++ xruns/cases/fpc/create_mesh.sh
@@ -1,7 +1,7 @@
 GMSH="gmsh"
 
 # geo=fpc_uns.geo
-geo=fpc_str_0.geo
-# geo=fpc_str_1.geo
+# geo=fpc_str_0.geo
+geo=fpc_str_1.geo
 
 ${GMSH} -format msh22 -2 "$geo" -o "./$(basename "${geo%.geo}").msh"
